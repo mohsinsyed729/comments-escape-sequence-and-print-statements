5bad22c60b4ac0bb20d89004e65913c635e00c67
mohsinsyed729 <136225413+mohsinsyed729@users.noreply.github.com>
2023-07-07 20:02:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+# for adding  another line, (use \n)
+print("my name is syed mohsin\nand my father's name is syed aijaz ahmad shah")
+
+
+# for keeping something in quotes we use \" and \" before and after that word or sentence. eg
+print("my name is \"syed mohsin\"\nand my father's name is \"syed aijaz ahmad shah\"")
+#or
+print("my name is \"syed\"")
+print("my name is \'syed'")
+
+
+#for commenting or uncommenting use (cntrl + /)
+#we can write a comment after line too .eg below
+print("hello ,welcome to the coding world")  #how are you?
+# for moving lines from up to down or down to up use (alt+ arrow up or down)
+
+
+# for comment we can use # or 1,3 singble/double quotes. eg
+'this is me'
+'''this is me'''
+"this is me"
+""" this is me"""
+#this is me
+
+#for multiple vlues 
+print("hey",6,10,)
+#for seperating above line
+print("hey",6,10,sep="~")
+print("hey",6,3,4,sep=",")
+#or
+print("hey",6,8,end="7933\n")
+print("mohsin")
+print("hey",6,8,sep="-",end="7933\n")
+print("mohsin")
